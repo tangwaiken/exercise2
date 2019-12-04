e1e92f84c5ebe01d01816d2fa0af1d987b0d076c
kentang <ken.tang6030@gmail.com>
2019-12-04 16:38:11 +0800

    sadasd

---
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.exercise2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import java.lang.Math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val calbutton: Button = findViewById(R.id.buttonCalculate)
+        calbutton.setOnClickListener { cal() }
+        val resetButton: Button = findViewById(R.id.buttonReset)
+        resetButton.setOnClickListener {reset()}
+    }
+    private fun cal(){
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val h: EditText = findViewById(R.id.editTextHeight)
+        val w: EditText = findViewById(R.id.editTextWeight)
+        val height: Double = h.text.toString().toDouble()
+        val weight: Double = w.text.toString().toDouble()
+        val result: Double = weight / pow((height/100),2.0)
+        val img: ImageView = findViewById(R.id.imageViewProfile)
+        val bmi: TextView = findViewById(R.id.textViewBMI)
+        if(result<18.5){
+            img.setImageResource(R.drawable.under)
+        }
+        else if(result>=18.5&&result<25){
+            img.setImageResource(R.drawable.normal)
+        }
+        else{
+            img.setImageResource(R.drawable.over)
+        }
+        bmi.setText(R.string.bmi.toString() + String.format("%.2f", result))
+    }
+    private fun reset(){
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val h: EditText = findViewById(R.id.editTextHeight)
+        val w: EditText = findViewById(R.id.editTextWeight)
+        val img: ImageView = findViewById(R.id.imageViewProfile)
+        val bmi: TextView = findViewById(R.id.textViewBMI)
+
+        h.setText("")
+        w.setText("")
+        img.setImageResource(R.drawable.empty)
+        bmi.setText(R.string.bmi)
+    }
+}
